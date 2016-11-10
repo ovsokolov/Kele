fe544dbca9f6172cc0fb0ca06a131f2ce1ddb3a7
Oleg Sokolov <olegsokolov@Olegs-Air.home>
2016-11-10 10:15:29 -0500

    Checkpoint Mentor Availability: completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -6,7 +6,7 @@ class Kele
   include HTTParty
   base_uri 'https://www.bloc.io/api/v1'
 
-  attr_accessor :auth_token
+  attr_accessor :auth_token, :student_data, :mentor_availability
 
   def initialize( name, password )
     result = self.class.post('/sessions', :body => {"email" => name, "password" => password})
@@ -16,6 +16,14 @@ class Kele
 
   def get_me
     result = self.class.get('/users/me', headers: { "authorization" => @auth_token })
-    JSON.parse(result.body)
+    @student_data = JSON.parse(result.body)
+  end
+
+  def get_mentor_availability
+    self.get_me if @student_data.nil?
+    mentor_id = @student_data["current_enrollment"]["mentor_id"]
+    url = "/mentors/#{mentor_id}/student_availability"
+    result = self.class.get(url, headers: { "authorization" => @auth_token }, :body => {"id" => mentor_id})
+    @mentor_availability = JSON.parse(result.body)
   end
 end
